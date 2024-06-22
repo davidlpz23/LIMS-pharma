b50e3f6bb9386b857b482822926420dc4e347022
David Lopez <gdavid.udeamail.com>
2024-06-21 20:48:54 -0500

    Refactor routing and add header and footer components
    This commit refactors the routing in the App.js file to include new routes for login, registration, and reagent management. It also adds the Header and Footer components to the main App component. These changes improve the navigation and user experience of the application.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,19 +5,23 @@ import Register from './components/Register';
 import SampleRegister from './components/SampleRegister';
 import ReagentRegister from './components/ReagentRegister';
 import ReagentList from './components/ReagentList'; 
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     
       <div className="App">
+        <Header />
         <Routes> 
-        <Route path="/" element={<ReagentList />} /> {/* Ruta raíz agregada aquí */}
+          <Route path="/" element={<Login />} /> // Cambia el componente por defecto a Login  
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />    
-          <Route path="/sample-register" element={<SampleRegister />} />   
-          <Route path="/reagent-register" element={<ReagentRegister />} />
-          <Route path="/reagent-list" element={<ReagentList />} />
+          <Route path="/path_to_SampleRegister" element={<SampleRegister />} /> {/* Agrega la ruta al componente SampleRegister.jsx */}
+          <Route path="/path_to_ReagenstRegister" element={<ReagentRegister />} /> {/* Agrega la ruta al componente ReagentRegister.jsx */}
+          <Route path="/path_to_ReagentList" element={<ReagentList />} /> {/* Agrega la ruta al componente ReagentList.jsx */}
         </Routes>
+        <Footer />
       </div>
     
   );
